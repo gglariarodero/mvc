7b8ad1b2a25f52f3fe0141a399c18b02f38f0994
gglariarodero <gglariarodero@gmail.com>
2026-02-22 21:37:25 +0100

    Limpiar comentarios

--- person/controllers/Personas_controller.php
+++ person/controllers/Personas_controller.php
@@ -6,7 +6,6 @@ class Personas_controller {
 
     public function __construct($pdo) {
 
-
         require_once  "models/Personas_model.php";
         require_once 'config/config.php';
         $this->personaModel = new Personas_model($pdo);
@@ -15,30 +14,23 @@ class Personas_controller {
 
     public function view(){
 
-
     $datos_personas = $this->personaModel->get_all_personas();
 
     $datos_finales = array();
     $fecha_actual = date("Y-m-d");
 
-    foreach ($datos_personas as $personas) {
+        foreach ($datos_personas as $personas) {
+               
+            $diferencia = abs(strtotime($fecha_actual) - strtotime($personas['birthdate']));
+            
+            $edad = floor($diferencia / (365*60*60*24));
+    
+            $personas['edad'] = $edad ;
+            $datos_finales [] = $personas;
+        }
 
-   
-        
-        $diferencia = abs(strtotime($fecha_actual) - strtotime($personas['birthdate']));
-        
-        $edad = floor($diferencia / (365*60*60*24));
-
-        $personas['edad'] = $edad ;
-        $datos_finales [] = $personas;
-    }
-
-   
-    //exit();
     require 'views/listado.php';
-    //$this->view('listado',  $datos_personas);
 
     }
-
 
 }
